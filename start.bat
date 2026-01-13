9be3991194ba90055d08e718186a5df46132a08b
Slavavw <slavavw1978@gmail.com>
2026-01-13 13:28:14 +0300

    добавил создание файла alttab вызов команды чтобы переключить фокус окна от браузера к окну кассира

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off            
-            SET EdgePath="C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
-            REM URL сайта для киоска
-            SET StartURL="http://192.168.0.116:3434/"
-            REM Запуск в режиме киоска
-            start "" %EdgePath%
-            exit
